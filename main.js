39668361947c9bf30b8c3117ce1d8f49052c09e9
theocerdan <cerdantheo.contact@gmail.com>
2023-01-04 17:37:11 +0100

    hey

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ var Engine = Matter.Engine,
 
 // create an engine
 var engine = Engine.create();
+var PAD_SPEED = 75;
+let ball;
 
 engine.world.gravity.y = 0;
 // create a renderer
@@ -21,8 +23,7 @@ var render = Render.create({
     width: 810,
     height: 800,
     pixelRatio: 1,
-    background: "#fafa",
-    wireframeBackground: "#222",
+    wireframeBackground: "#000000",
     hasBounds: true,
     enabled: true,
     wireframes: true,
@@ -30,8 +31,8 @@ var render = Render.create({
     showDebug: false,
     showBroadphase: false,
     showBounds: false,
-    showVelocity: false,
-    showCollisions: false,
+    showVelocity: true,
+    showCollisions: true,
     showSeparations: false,
     showAxes: false,
     showPositions: true,
@@ -45,27 +46,18 @@ var render = Render.create({
   },
 });
 // create two boxes and a ground
-var ball = Bodies.circle(200, 200, 40, 40);
-ball.restitution = 0.8;
-ball.inertia = Infinity;
-ball.friction = 0;
-ball.frictionAir = 0;
-ball.frictionStatic = 0;
 
-var paddleL = Bodies.rectangle(0, 200, 40, 160, { isStatic: true });
-var paddleR = Bodies.rectangle(810, 200, 40, 160, { isStatic: true });
+var paddleL = Bodies.rectangle(0, 200, 20, 160, { isStatic: true });
+var paddleR = Bodies.rectangle(810, 200, 20, 160, { isStatic: true });
 
-var wallT = Bodies.rectangle(0, 0, 8000, 50, { isStatic: true });
+var wallT = Bodies.rectangle(0, 0, 8000, 50, {
+  isStatic: true,
+  label: "wallT",
+});
 var wallB = Bodies.rectangle(0, 800, 8000, 50, { isStatic: true });
 
-Matter.Body.applyForce(
-  ball,
-  { x: ball.position.x, y: ball.position.y },
-  { x: -0.1, y: 0 }
-);
-
 // add all of the bodies to the world
-Composite.add(engine.world, [paddleL, paddleR, ball, wallT, wallB]);
+Composite.add(engine.world, [paddleL, paddleR, wallT, wallB]);
 
 // run the renderer
 Render.run(render);
@@ -78,34 +70,71 @@ Runner.run(runner, engine);
 
 setInterval(function () {
   Engine.update(engine, 1000 / 60);
+  Matter.Events.on(engine, "collisionStart", (e) => {
+    console.log(e.pairs[0]);
+    //console.log("mathieu");
+  });
+  //console.log(ball.position.x + " - " + ball.position.y);
+  if (ball != undefined)
+    if (ball.position.y > 720 || ball.position.y < 80) {
+      console.log("wall hit");
+      let rejectForce = -(ball.velocity.y * 0.05);
+      //console.log(rejectForce);
+      if (rejectForce > 1) rejectForce = 1;
+      Matter.Body.applyForce(
+        ball,
+        { x: ball.position.x, y: ball.position.y },
+        { x: 0, y: rejectForce }
+      );
+    }
 }, 1000 / 60);
 
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == "40") {
     Matter.Body.setPosition(paddleR, {
       x: paddleR.position.x,
-      y: paddleR.position.y + 50,
+      y: paddleR.position.y + PAD_SPEED,
     });
   }
 
   if (e.keyCode == "38") {
     Matter.Body.setPosition(paddleR, {
       x: paddleR.position.x,
-      y: paddleR.position.y - 50,
+      y: paddleR.position.y - PAD_SPEED,
     });
   }
 
   if (e.keyCode == "65") {
     Matter.Body.setPosition(paddleL, {
       x: paddleL.position.x,
-      y: paddleL.position.y + 50,
+      y: paddleL.position.y + PAD_SPEED,
     });
   }
 
   if (e.keyCode == "81") {
     Matter.Body.setPosition(paddleL, {
       x: paddleL.position.x,
-      y: paddleL.position.y - 50,
+      y: paddleL.position.y - PAD_SPEED,
     });
+  }
+  if (e.keyCode == "32") {
+    console.log("game start");
+    var b = Bodies.circle(200, 200, 40, 40);
+    b.restitution = 1.1;
+    b.inertia = Infinity;
+    b.friction = 0;
+    b.frictionAir = 0;
+    b.frictionStatic = 0;
+    Matter.Body.setPosition(b, {
+      x: 500,
+      y: 500,
+    });
+    Matter.Body.applyForce(
+      b,
+      { x: b.position.x, y: b.position.y },
+      { x: -0.05, y: 0.01 }
+    );
+    Composite.add(engine.world, [b]);
+    ball = b;
   }
 });
