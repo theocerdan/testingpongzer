f9713f0bfdddddaa824007c8e4feb2a65cffd235
mbaurin <1mathieu.baurin@gmail.com>
2023-01-09 18:37:12 +0100

    refractoring

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import "./style.css";
 import Matter from "matter-js";
 
-// module aliases
+/// MODULE ALIASES ///
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
 
-// create an engine
+/// ENGINE CREATION ///
+
 var engine = Engine.create();
 engine.world.gravity.y = 0;
 
-let ball;
+/// CONSTANTS ///
+
 const PAD_SPEED = 12;
 const GAME_HEIGHT = 650;
 const GAME_WIDTH = 850;
@@ -20,7 +23,8 @@ const BALL_RADIUS = 15;
 const WALL_THICKNESS = 20;
 const PADDLE_LENGHT = 150;
 
-// create a renderer
+/// RENDERER CREATION ///
+
 var render = Render.create({
   element: document.querySelector("#gameBoard"),
   engine: engine,
@@ -32,16 +36,16 @@ var render = Render.create({
     hasBounds: true,
     enabled: true,
     wireframes: false,
-    showSleeping: true,
+    showSleeping: false,
     showDebug: false,
     showBroadphase: false,
     showBounds: false,
-    showVelocity: true,
-    showCollisions: true,
+    showVelocity: false,
+    showCollisions: false,
     showSeparations: false,
     showAxes: false,
-    showPositions: true,
-    showAngleIndicator: true,
+    showPositions: false,
+    showAngleIndicator: false,
     showIds: false,
     showShadows: false,
     showVertexNumbers: false,
@@ -51,7 +55,19 @@ var render = Render.create({
   },
 });
 
-// create 2 paddles
+/// RUN THE RENDERER ///
+
+Render.run(render);
+
+/// VARIABLES /// 
+
+let ball;
+var currentScoreLeft = 0;
+var currentScoreRight = 0;
+var keys = [];
+
+/// CREATION OF ALL BODIES ///
+
 var paddleL = Bodies.rectangle(20, GAME_HEIGHT / 2, 20, PADDLE_LENGHT, { 
   isStatic: true,
   label: "PaddleLeft",
@@ -66,8 +82,6 @@ var paddleR = Bodies.rectangle(GAME_WIDTH - 20, GAME_HEIGHT / 2, 20, PADDLE_LENG
     fillStyle: "white"
   }
 });
-
-// create a top and bottom walls
 var wallT = Bodies.rectangle(0, 0, GAME_WIDTH * 2, WALL_THICKNESS, {
   isStatic: true,
   label: "WallT",
@@ -83,73 +97,63 @@ var wallB = Bodies.rectangle(0, GAME_HEIGHT, GAME_WIDTH * 2, WALL_THICKNESS, {
   }
 });
 
-// add all of the bodies to the world
+/// ADD BODIES TO THE WORLD ///
 Composite.add(engine.world, [paddleL, paddleR, wallT, wallB]);
 
-// run the renderer
-Render.run(render);
+/// CREATE AND RUN THE RUNNER ///
 
-// create runner
 var runner = Runner.create();
-
-// run the engine
 Runner.run(runner, engine);
 
-spawnBall();
+initBall();
+
+/// GAME LOOP ///
 
 setInterval(function () {
   updateMovePaddle();
   if (ball != undefined) {
-    Matter.Events.on(engine, "collisionStart", (e) => {
-      const pairs = e.pairs
-      const objA = pairs[0].bodyA.label
-      const objB = pairs[0].bodyB.label
-      if (objB === 'Ball' && (objA === 'WallB' || objA === 'WallT')) {
-        setTimeout(() => {
-          let velocity = ball.velocity.y
-          if (Math.abs(velocity) < 1)
-            velocity = velocity + Math.sign(velocity)
-          Matter.Body.setVelocity(ball, { x: ball.velocity.x, y: velocity })
-        }, 1000 / 60)
-      }
-      if (objB === 'Ball' && (objA === 'PaddleRight' || objA === 'PaddleLeft')) {
-        setTimeout(() => {
-          let velocity = ball.velocity.y
-          if (Math.abs(velocity) < 1)
-            velocity = velocity + Math.sign(velocity)
-          Matter.Body.setVelocity(ball, { x: ball.velocity.x, y: velocity })
-        }, 1000 / 120)
-      }
-    });
-    if (ball.position.x < 0 || ball.position.x > GAME_WIDTH) {
-      updateScore(ball.position);
-      resetBall(ball)
-    }
+    gestionCollisions();
+    gestionScore();
     Engine.update(engine, 1000 / 120);
   }
 }, 1000 / 60);
 
-function randomNumberInterval(min, max) { 
-  let x = Math.floor(Math.random() * (max - min) + min);
-  if (x == 0)
-    return randomNumberInterval(min, max);
-  return x;
+/// COLLISION FUNCTIONS ///
+
+function gestionCollisions() {
+  Matter.Events.on(engine, "collisionStart", (e) => {
+    const objA = e.pairs[0].bodyA.label
+    const objB = e.pairs[0].bodyB.label
+    if (objB === 'Ball' && (objA === 'WallB' || objA === 'WallT')) {
+      setTimeout(() => {
+        calculNewVelocityCollisions();
+      }, 1000 / 60)
+    }
+    if (objB === 'Ball' && (objA === 'PaddleRight' || objA === 'PaddleLeft')) {
+      setTimeout(() => {
+        calculNewVelocityCollisions();
+      }, 1000 / 120)
+    }
+  });
 }
 
-function launchBall(ball) {
-  let starter = randomNumberInterval(1, 3)
-  starter == 1 ? starter = 1 : starter = -1;
-  Matter.Body.setVelocity(ball, { x: 3 * starter, y: randomNumberInterval(-4, 5) })
+function calculNewVelocityCollisions() {
+  let velocity = ball.velocity.y
+  if (Math.abs(velocity) < 1)
+    velocity = velocity + Math.sign(velocity)
+  Matter.Body.setVelocity(ball, { x: ball.velocity.x, y: velocity })
 }
 
-function spawnBall() {
+/// BALL FUNCTIONS MANAGER ///
+
+function initBall() {
   var b = Bodies.circle(200, 200, BALL_RADIUS, {
     label: "Ball",
     render : {
       fillStyle: "white"
     }
   });
-  b.restitution = 1.08;
+  b.restitution = 1.09;
   b.inertia = Infinity;
   b.friction = 0;
   b.frictionAir = 0;
@@ -161,12 +165,18 @@ function spawnBall() {
   ball = b;
 }
 
+function launchBall() {
+  let starter = randomNumberInterval(1, 3)
+  starter == 1 ? starter = 1 : starter = -1;
+  Matter.Body.setVelocity(ball, { x: 3 * starter, y: randomNumberInterval(-4, 5) })
+}
+
 function resetBall() {
   Matter.Body.setPosition(ball, {x: GAME_WIDTH / 2, y: GAME_HEIGHT / 2,});
   Matter.Body.setVelocity(ball, { x: 0, y:0 })
 }
 
-var keys = [];
+/// KEY MANAGEMENT ///
 
 document.body.addEventListener("keydown", function (e) {
   keys[e.keyCode] = true;
@@ -192,18 +202,30 @@ function updateMovePaddle() {
 
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == "32" && (ball.velocity.x == 0 && ball.velocity.y == 0))
-    launchBall(ball);
+    launchBall();
 });
 
-////////SCORE GESTION////////
+/// SCORE GESTION ///
 
-var currentScoreLeft = 0;
-var currentScoreRight = 0;
+function gestionScore() {
+  if (ball.position.x < 0 || ball.position.x > GAME_WIDTH) {
+    updateScore();
+    resetBall()
+  }
+}
 
-function updateScore(ball) {
-  console.log(ball);
-  if (ball.x >= GAME_WIDTH)
+function updateScore() {
+  if (ball.position.x >= GAME_WIDTH)
     document.getElementById('score left span').innerHTML = ++currentScoreLeft;
-  else if (ball.x <= 0)
+  else if (ball.position.x <= 0)
     document.getElementById('score right span').innerHTML = ++currentScoreRight;
+}
+
+/// UTILS ///
+
+function randomNumberInterval(min, max) { 
+  let x = Math.floor(Math.random() * (max - min) + min);
+  if (x == 0)
+    return randomNumberInterval(min, max);
+  return x;
 }
